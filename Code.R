dd170a81514901364e1905056a16e527f54fb6f0
Nanako Ohashi <nanako.ohashi@gmail.com>
2019-07-25 21:53:43 -0600

    feat: update code

--- Code.R
+++ Code.R
@@ -10,3 +10,4 @@ library("broom", lib.loc="~/R/win-library/3.6")
 # Import .csv file
 population.csv <- read.csv("C:/Users/Nanako/Desktop/R/nst-est2016-01.csv", header = TRUE)
 str(population.csv)
+View(population.csv)
